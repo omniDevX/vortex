4cbb24be52f90202ac5f41281ee2e9527ebb75e8
aiaccfin <aiaccfin@gmail.com>
2025-07-24 13:07:11 -0400

    update

---
+++ equation.js
@@ -0,0 +1,35 @@
+// let equations = [
+// 	"foo = 1",
+// 	"bar = foo + 1",
+// 	"baz = bar + foo + 3",
+// 	"faz = baz + 4"
+// ]
+
+
+// eval("var x = 'Hello from eval!';");
+
+// console.log(x);
+
+
+var MyVariable = "Value of variable";
+console.log(window["MyVariable"]);
+
+// output = {
+// 	"foo": 1,
+// 	"bar" : 2,
+// 	"baz" : 6,
+// 	"faz": 10
+// }
+
+
+// eval("var x = 'Hello from eval!';");
+
+// function test(equations) {
+
+//     // let results = {};
+//     // for (let equation of equations) {
+//     //     let [key, expression] = equation.split(" = ");
+//     //     results
+
+
+// // output = function(equations) {e
